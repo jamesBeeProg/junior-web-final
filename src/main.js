69e85904938e9376dbab35b0aee83e58d972d5d6
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-05-11 07:47:34 -0400

    better stuff

---
+++ src/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+document.querySelectorAll('.drop-down-button').forEach((button) => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+        button.parentElement.classList.toggle('drop-down-active');
+        if (button.parentElement.classList.contains('drop-down-active')) {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        } else {
+            content.style.maxHeight = 0;
+        }
+    });
+});
