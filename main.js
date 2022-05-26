3e6b18ee0be9ebfc077e431d7340de2f70b07210
jamesBeeProg <jamesBeeProg@users.noreply.github.com>
2022-05-26 12:37:46 +0000

    deploy: e07dd398a12aefc791054a524192ac7c1fee368f

--- main.js
+++ main.js
@@ -15,3 +15,35 @@ document.querySelectorAll('.drop-down-button').forEach((button) => {
 burger.addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('burger-active');
 });
+
+const sidebar = {
+    james: 'James',
+    felipe: 'Felipe',
+    wyatt: 'Wyatt',
+    eduardo: 'Eduardo',
+    lucas: 'Lucas',
+    eric: 'Eric',
+    jaxan: 'Jaxan',
+    ryang: 'Ryan G',
+    austin: 'Austin',
+    tye: 'Tye',
+    kyleS: 'Kyle S',
+    leandro: 'Leandro',
+    shea: 'Shea',
+};
+
+const navbar = document.querySelector('body > nav');
+
+for (const [path, text] of Object.entries(sidebar)) {
+    if (!location.href.includes(`/${path}/`)) continue;
+
+    const query = `//button[contains(., '${text}')]`;
+    const button = document.evaluate(query, navbar).iterateNext();
+
+    button.parentElement.classList.add('drop-down-active');
+
+    const content = button.nextElementSibling;
+    content.style.maxHeight = content.scrollHeight + 'px';
+
+    break;
+}
